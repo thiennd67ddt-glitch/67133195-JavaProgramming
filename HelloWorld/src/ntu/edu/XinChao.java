0e33080efc0637ec343f33dbf60836a32adc1383
thiennd67ddt-glitch <thien.nd.67ddt@ntu.edu.vn>
2025-11-14 21:40:19 +0700

    Ví dụ 1
    Demo

---
+++ HelloWorld/src/ntu/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ntu.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Tôi Lần Đầu Học JAVA, và tôi đang thử nghiệm quản lý mã nguồn"");
+	}
+
+}
